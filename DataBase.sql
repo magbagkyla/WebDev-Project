5544a342037044dba67d292af554a0ab9e4077a8
magbagkyla <magbagkyla05@gmail.com>
2025-03-20 22:22:01 +0800

    Create DataBase.sql

---
+++ DataBase.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `crud_table` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(250) NOT NULL,
+  `AGE` varchar(250) NOT NULL,
+  `ADDRESS` varchar(250) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
